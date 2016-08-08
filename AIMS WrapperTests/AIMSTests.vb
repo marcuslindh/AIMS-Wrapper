32f512cd4fe849b4095ea55020f00c5566c1ce5c
marcus lindh <marcusl1@hotmail.com>
2016-08-08 12:35:06 +0200

    Added tests

---
+++ AIMS WrapperTests/AIMSTests.vb
@@ -0,0 +1,48 @@
+﻿Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+Imports AIMS
+
+Namespace AIMS.Tests
+    <TestClass()> Public Class AIMSTests
+
+        Private Property _Login As New LoginSettings
+        Public ReadOnly Property Login As LoginSettings
+            Get
+                If _Login.Username = "" Then
+                    If IO.File.Exists("c:\temp\AIMS_Wrapper_Login.txt") Then
+                        Dim text As String = My.Computer.FileSystem.ReadAllText("c:\temp\AIMS_Wrapper_Login.txt")
+                        Dim lines() As String = text.Split(vbCrLf)
+                        _Login.Username = lines(0).Trim()
+                        _Login.Password = lines(1).Trim()
+                        _Login.Site = lines(2).Trim()
+                    End If
+                End If
+                Return _Login
+            End Get
+        End Property
+
+        <TestMethod()> Public Sub GetServerVersionTest()
+            Dim AIMSServer As New Server
+            AIMSServer.Login(Login.Username, Login.Password, Login.Site)
+
+            Assert.AreEqual(AIMSServer.GetServerVersion.Substring(0, 2), "2.")
+        End Sub
+
+        <TestMethod()> Public Sub GetServerInfo()
+            Dim AIMSServer As New Server
+            AIMSServer.Login(Login.Username, Login.Password, Login.Site)
+
+            Dim info = AIMSServer.GetServerInfo
+
+            Assert.AreEqual(info.Status, "Online")
+        End Sub
+    End Class
+
+
+End Namespace
+
+Public Class LoginSettings
+    Public Property Username As String = ""
+    Public Property Password As String = ""
+    Public Property Site As String = ""
+End Class
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # AIMS Wrapper
 Autodesk Infrastructure Map Server API Wrapper
 
+## Tested
+It is tested on Autodesk Infrastructure Map Server 2015
